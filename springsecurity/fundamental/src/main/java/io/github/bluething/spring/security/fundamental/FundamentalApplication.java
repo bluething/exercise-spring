f5f466cefd032d1b14b0eea2dce4931622705940
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-09-29 22:03:21 +0700

    Init spring security fundamental project

---
+++ springsecurity/fundamental/src/main/java/io/github/bluething/spring/security/fundamental/FundamentalApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.spring.security.fundamental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentalApplication.class, args);
+	}
+
+}
