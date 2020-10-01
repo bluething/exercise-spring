8752ea30d795328c2cac08ae5545e4b634a29a22
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-10-01 13:59:47 +0700

    Add book controller

---
+++ springsecurity/fundamental/src/main/java/io/github/bluething/spring/security/fundamental/book/BookController.java
@@ -0,0 +1,25 @@
+package io.github.bluething.spring.security.fundamental.book;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping(path = "/book/{isbn}")
+    public ResponseEntity<Book> getBook(@PathVariable(name = "isbn") String isbn) {
+        return new ResponseEntity<Book>(new Book(isbn, "", ""), HttpStatus.OK);
+    }
+
+}
---
+++ springsecurity/fundamental/src/main/java/io/github/bluething/spring/security/fundamental/book/BookRepository.java
@@ -0,0 +1,29 @@
+package io.github.bluething.spring.security.fundamental.book;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class BookRepository {
+
+    public Book getBook(Book bookRequest) {
+        return new Book(bookRequest.getIsbn(), "This is the author", "This is a title");
+    }
+
+    public List<Book> getBooks() {
+        List<Book> books = new ArrayList<>();
+        books.add(new Book("978-6-2021-1578-0", "This is the author", "This is a title"));
+        books.add(new Book("978-0-4546-1715-3", "This is the author", "This is a title"));
+        return books;
+    }
+
+    public boolean deleteBook(Book bookRequest) {
+        return true;
+    }
+
+    public Book addBook(Book bookRequest) {
+        return bookRequest;
+    }
+}
