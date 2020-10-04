ef1603cd973cdbafb4529871a6de55a91c7fb534
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-10-04 17:34:08 +0700

    Remove unused import

--- springsecurity/fundamentaljwt/src/main/java/io/github/bluething/spring/security/fundamentaljwt/jwt/JwtToken.java
+++ springsecurity/fundamentaljwt/src/main/java/io/github/bluething/spring/security/fundamentaljwt/jwt/JwtToken.java
@@ -7,7 +7,6 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
