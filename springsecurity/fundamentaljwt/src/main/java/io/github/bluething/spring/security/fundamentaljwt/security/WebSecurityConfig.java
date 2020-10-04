d29e63d9cc58dd5d0897738d1218fa6bc495946f
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-10-04 14:49:39 +0700

    Add user detail service configuration

--- springsecurity/fundamentaljwt/src/main/java/io/github/bluething/spring/security/fundamentaljwt/security/WebSecurityConfig.java
+++ springsecurity/fundamentaljwt/src/main/java/io/github/bluething/spring/security/fundamentaljwt/security/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -18,6 +19,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new JpaUserDetailService();
     }
 
 }
