6cfdb66d7451b078d2339e79de7bfe433296611b
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-10-04 15:15:27 +0700

    Add configuration
    Disable csrf, we will get 403 if not.
    Permit request to /api/auth/signin

--- springsecurity/fundamentaljwt/src/main/java/io/github/bluething/spring/security/fundamentaljwt/security/WebSecurityConfig.java
+++ springsecurity/fundamentaljwt/src/main/java/io/github/bluething/spring/security/fundamentaljwt/security/WebSecurityConfig.java
@@ -3,7 +3,9 @@ package io.github.bluething.spring.security.fundamentaljwt.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -24,6 +26,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public UserDetailsService userDetailsService() {
         return new JpaUserDetailService();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests()
+                .antMatchers("/api/auth/sigin").permitAll()
+                .anyRequest().authenticated();
     }
 
 }
