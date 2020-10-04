2d7d77ec3216122b70dc5a783892e9ab40f3bfa6
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-10-04 17:24:26 +0700

    Delete Component annotation
    Creating bean at configuration class

--- springsecurity/fundamentaljwt/src/main/java/io/github/bluething/spring/security/fundamentaljwt/jwt/JwtSecret.java
+++ springsecurity/fundamentaljwt/src/main/java/io/github/bluething/spring/security/fundamentaljwt/jwt/JwtSecret.java
@@ -2,11 +2,9 @@ package io.github.bluething.spring.security.fundamentaljwt.jwt;
 
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 
-@Component
 public class JwtSecret {
 
     public SecretKey getRandomKey() {
