508579577251e26b0b0aee2bbe9f3941a4393f7e
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-10-04 10:38:15 +0700

    Disable default user detail service
    Default user detail service configure via UserDetailsServiceAutoConfiguration which is have ConditionalOnBean(AuthenticationManager.class). So if we don't have AuthenticationManager at beginning, the default user detail service won't created.

---
+++ springsecurity/fundamentaljwt/src/main/java/io/github/bluething/spring/security/fundamentaljwt/security/WebSecurityConfig.java
@@ -0,0 +1,16 @@
+package io.github.bluething.spring.security.fundamentaljwt.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+}
