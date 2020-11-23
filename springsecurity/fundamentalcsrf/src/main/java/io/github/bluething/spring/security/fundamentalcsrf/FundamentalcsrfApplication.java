e718df503dc79ae9934e9d5c8b2048937370e08a
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-11-23 14:10:41 +0700

    Init fundamental csrf

---
+++ springsecurity/fundamentalcsrf/src/main/java/io/github/bluething/spring/security/fundamentalcsrf/FundamentalcsrfApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.spring.security.fundamentalcsrf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalcsrfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentalcsrfApplication.class, args);
+	}
+
+}
---
+++ springsecurity/fundamentalcsrf/src/test/java/io/github/bluething/spring/security/fundamentalcsrf/FundamentalcsrfApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.spring.security.fundamentalcsrf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentalcsrfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
