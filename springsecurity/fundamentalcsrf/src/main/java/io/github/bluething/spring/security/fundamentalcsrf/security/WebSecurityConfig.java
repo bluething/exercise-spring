fbd1931dbcacea6bbb02e0f5fd1a764da14410f8
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-11-23 14:57:09 +0700

    Simulate malicious attack

---
+++ springsecurity/fundamentalcsrf/src/main/java/io/github/bluething/spring/security/fundamentalcsrf/security/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package io.github.bluething.spring.security.fundamentalcsrf.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        // GENERALLY DON'T DO THIS!
+        //http.csrf().disable();
+
+
+    }
+}
--- springsecurity/fundamentalcsrf/src/main/java/io/github/bluething/spring/security/fundamentalcsrf/web/MainController.java
+++ springsecurity/fundamentalcsrf/src/main/java/io/github/bluething/spring/security/fundamentalcsrf/web/MainController.java
@@ -1,13 +1,21 @@
 package io.github.bluething.spring.security.fundamentalcsrf.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@RestController
+@Controller
 public class MainController {
 
     @GetMapping
     public String main() {
+        return "main.html";
+    }
+
+    // POST, PUT, DELETE mutating actions in REST
+    @PostMapping("/change")
+    public String doSomething() {
+        System.out.println(":(");
         return "main.html";
     }
 }
