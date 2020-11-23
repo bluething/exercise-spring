c84bb6e266f79ca95db930479e34ded17550cb37
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-11-23 15:13:41 +0700

    Init fundamental cors

---
+++ springsecurity/fundamentalcors/src/test/java/io/github/bluething/spring/security/fundamentalcors/FundamentalcorsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.spring.security.fundamentalcors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentalcorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
