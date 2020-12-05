be14991f2bf16be31550e413e093401f940c212c
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-12-05 16:58:54 +0700

    Add cors config

--- springsecurity/fundamentalcors/src/main/java/io/github/bluething/spring/security/fundamentalcors/security/WebSecurityConfig.java
+++ springsecurity/fundamentalcors/src/main/java/io/github/bluething/spring/security/fundamentalcors/security/WebSecurityConfig.java
@@ -3,13 +3,28 @@ package io.github.bluething.spring.security.fundamentalcors.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+
         http.authorizeRequests()
                 .anyRequest().permitAll();
+
+        http.cors(c -> {
+            CorsConfigurationSource cs = r -> {
+                CorsConfiguration cc =  new CorsConfiguration();
+                cc.setAllowedOrigins(List.of("*"));
+                cc.setAllowedMethods(List.of("GET", "POST"));
+                return cc;
+            };
+            c.configurationSource(cs);
+        });
     }
 }
