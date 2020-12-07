e785b9d7c512a395ecfe3c6f9315085f0f3bac34
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-12-07 14:07:04 +0700

    Add user management

---
+++ springsecurity/fundamentaloauth2/oauth2basic/src/main/java/io/github/bluething/spring/security/fundamentaloauth2/oauth2basic/security/UserManagementConfig.java
@@ -0,0 +1,48 @@
+package io.github.bluething.spring.security.fundamentaloauth2.oauth2basic.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+@Configuration
+public class UserManagementConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetailsManager udm = new InMemoryUserDetailsManager();
+
+        UserDetails user = User.withUsername("fulan")
+                .password("12345")
+                .authorities("read")
+                .build();
+        udm.createUser(user);
+
+        return udm;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+
+}
