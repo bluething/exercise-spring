9741d271a6d205c1b7d12590ae21873cbed34d3f
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-12-07 14:49:47 +0700

    Add auth config
    For client to connect to auth server

---
+++ springsecurity/fundamentaloauth2/oauth2basic/src/main/java/io/github/bluething/spring/security/fundamentaloauth2/oauth2basic/security/AuthServerConfig.java
@@ -0,0 +1,36 @@
+package io.github.bluething.spring.security.fundamentaloauth2.oauth2basic.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public AuthServerConfig(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("client1")
+                .secret("secret1")
+                .scopes("read")
+                .authorizedGrantTypes("password");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
+
+}
