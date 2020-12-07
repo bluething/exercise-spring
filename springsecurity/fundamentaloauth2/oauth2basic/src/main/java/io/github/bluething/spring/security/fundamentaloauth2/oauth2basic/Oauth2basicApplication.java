c986b7c395d0647f78324a7fb73ed15572719842
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-12-07 14:05:48 +0700

    Init oauth2 basic

---
+++ springsecurity/fundamentaloauth2/oauth2basic/src/main/java/io/github/bluething/spring/security/fundamentaloauth2/oauth2basic/Oauth2basicApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.spring.security.fundamentaloauth2.oauth2basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2basicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2basicApplication.class, args);
+    }
+
+}
---
+++ springsecurity/fundamentaloauth2/oauth2basic/src/test/java/io/github/bluething/spring/security/fundamentaloauth2/oauth2basic/Oauth2basicApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.spring.security.fundamentaloauth2.oauth2basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2basicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
