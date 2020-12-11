58b8e7b1980d0a0bef38d71799522d38c1aff4d5
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-12-11 16:38:39 +0700

    Init grant type

---
+++ springsecurity/fundamentaloauth2/granttype/src/main/java/io/github/bluething/spring/security/fundamentaloauth2/granttype/GranttypeApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.spring.security.fundamentaloauth2.granttype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GranttypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GranttypeApplication.class, args);
+    }
+
+}
---
+++ springsecurity/fundamentaloauth2/granttype/src/test/java/io/github/bluething/spring/security/fundamentaloauth2/granttype/GranttypeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.spring.security.fundamentaloauth2.granttype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GranttypeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
