4d38affc38021edce4cccaa0e24e5f6c29c7b677
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-12-27 20:21:26 +0700

    Init project

---
+++ logging/structuredlogging/src/test/java/io/github/bluething/spring/logging/structuredlogging/StructuredloggingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.spring.logging.structuredlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StructuredloggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
