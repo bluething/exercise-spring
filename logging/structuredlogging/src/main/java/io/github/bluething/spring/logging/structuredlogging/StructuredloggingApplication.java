f45c76f76a60ca26052e9c7b293ac83466442f62
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-12-28 14:21:45 +0700

    Add more sample log

--- logging/structuredlogging/src/main/java/io/github/bluething/spring/logging/structuredlogging/StructuredloggingApplication.java
+++ logging/structuredlogging/src/main/java/io/github/bluething/spring/logging/structuredlogging/StructuredloggingApplication.java
@@ -1,11 +1,16 @@
 package io.github.bluething.spring.logging.structuredlogging;
 
+import net.logstash.logback.argument.StructuredArguments;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
+
+import static net.logstash.logback.argument.StructuredArguments.v;
+import static net.logstash.logback.argument.StructuredArguments.value;
+import static net.logstash.logback.marker.Markers.append;
 
 @SpringBootApplication
 public class StructuredloggingApplication {
@@ -25,6 +30,10 @@ public class StructuredloggingApplication {
         } catch (NullPointerException nEx) {
             logger.error("I show when level is ERROR", nEx);
         }
+
+        logger.info("Additional info", v("name", "fulan"));
+        logger.info("Current milis {} ms", value("now", System.currentTimeMillis()));
+        logger.info(append("name2", "fulanah"), "log message");
     }
 
 }
