398fcf4f5c52747d7ca50028dda258d0e965aaa2
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-12-28 14:38:06 +0700

    Init project

---
+++ logging/structuredlogginginrestapi/src/main/java/io/github/bluething/spring/logging/structuredlogginginrestapi/StructuredlogginginrestapiApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.spring.logging.structuredlogginginrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructuredlogginginrestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StructuredlogginginrestapiApplication.class, args);
+    }
+
+}
---
+++ logging/structuredlogginginrestapi/src/test/java/io/github/bluething/spring/logging/structuredlogginginrestapi/StructuredlogginginrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.spring.logging.structuredlogginginrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StructuredlogginginrestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
