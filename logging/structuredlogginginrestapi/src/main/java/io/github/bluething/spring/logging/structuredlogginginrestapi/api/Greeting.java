1dee88ce439060816a4a880c905b29784622fdb5
Habib Ali Machpud <habib.gunadarma@gmail.com>
2021-01-10 14:29:30 +0700

    Add basic log

--- logging/structuredlogginginrestapi/src/main/java/io/github/bluething/spring/logging/structuredlogginginrestapi/api/Greeting.java
+++ logging/structuredlogginginrestapi/src/main/java/io/github/bluething/spring/logging/structuredlogginginrestapi/api/Greeting.java
@@ -1,13 +1,18 @@
 package io.github.bluething.spring.logging.structuredlogginginrestapi.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Greeting {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(Greeting.class);
+
     @GetMapping(path = "/hii")
     public String sayHii() {
+        LOGGER.info("entering GET handler");
         return "Hii";
     }
 
