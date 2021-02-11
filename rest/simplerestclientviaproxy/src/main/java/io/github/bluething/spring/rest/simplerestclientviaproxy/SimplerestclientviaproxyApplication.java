b7acb162dc4f20b48378604ad23a588d6a2f461a
Habib Ali Machpud <habib.gunadarma@gmail.com>
2021-02-11 08:28:50 +0700

    Init

---
+++ rest/simplerestclientviaproxy/src/main/java/io/github/bluething/spring/rest/simplerestclientviaproxy/SimplerestclientviaproxyApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.spring.rest.simplerestclientviaproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplerestclientviaproxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplerestclientviaproxyApplication.class, args);
+    }
+
+}
---
+++ rest/simplerestclientviaproxy/src/test/java/io/github/bluething/spring/rest/simplerestclientviaproxy/SimplerestclientviaproxyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.spring.rest.simplerestclientviaproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplerestclientviaproxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
