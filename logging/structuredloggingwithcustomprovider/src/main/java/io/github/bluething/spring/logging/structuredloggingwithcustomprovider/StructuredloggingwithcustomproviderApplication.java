be56c7c8f55cb6059d9cb2238c1853926294df41
Habib Ali Machpud <habib.gunadarma@gmail.com>
2021-02-28 21:03:14 +0700

    Init custom provider

---
+++ logging/structuredloggingwithcustomprovider/src/main/java/io/github/bluething/spring/logging/structuredloggingwithcustomprovider/StructuredloggingwithcustomproviderApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.spring.logging.structuredloggingwithcustomprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructuredloggingwithcustomproviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StructuredloggingwithcustomproviderApplication.class, args);
+    }
+
+}
---
+++ logging/structuredloggingwithcustomprovider/src/test/java/io/github/bluething/spring/logging/structuredloggingwithcustomprovider/StructuredloggingwithcustomproviderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.spring.logging.structuredloggingwithcustomprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StructuredloggingwithcustomproviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
