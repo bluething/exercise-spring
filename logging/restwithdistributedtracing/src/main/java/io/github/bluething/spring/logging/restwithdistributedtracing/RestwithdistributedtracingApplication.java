98a8f5c646cc893170da8f83374235a0dd2b6801
Habib Ali Machpud <habib.gunadarma@gmail.com>
2021-03-18 09:12:03 +0700

    Init

---
+++ logging/restwithdistributedtracing/src/main/java/io/github/bluething/spring/logging/restwithdistributedtracing/RestwithdistributedtracingApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.spring.logging.restwithdistributedtracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestwithdistributedtracingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestwithdistributedtracingApplication.class, args);
+    }
+
+}
